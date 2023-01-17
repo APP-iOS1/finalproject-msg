ca4f46551732063616628fb0723cca904c12d6f8
zooey <popularyg@gmail.com>
2023-01-17 11:37:51 +0900

    Add: LoginView생성 및 기본UI #2

---
+++ MSG/MSG/View/LoginView.swift
@@ -0,0 +1,106 @@
+//
+//  LoginView.swift
+//  MSG
+//
+//  Created by zooey on 2023/01/17.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        
+        ZStack {
+            Color("Background")
+                .ignoresSafeArea()
+            VStack {
+                HStack(spacing: 20) {
+                    Image(systemName: "flag.checkered.2.crossed")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 70)
+                    VStack(alignment: .leading) {
+                        Text("MSG")
+                            .font(.largeTitle)
+                        Text("Money Save Game")
+                    }
+                }
+                .padding()
+                .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                .frame(maxHeight: UIScreen.main.bounds.height / 7)
+                
+                
+                
+                VStack(spacing: 20) {
+                    HStack {
+                        Text("회원가입")
+                            .bold()
+                        Divider()
+                            .frame(height: UIScreen.main.bounds.height / 30)
+                        Text("로그인")
+                            .bold()
+                    }
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.leading)
+                    
+                    Text("애플 로그인")
+                        .frame(width: 340, height: 40)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke()
+                            
+                        }
+                    Text("구글 로그인")
+                        .frame(width: 340, height: 40)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke()
+                            
+                        }
+                    Text("카카오 로그인")
+                        .frame(width: 340, height: 40)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke()
+                            
+                        }
+                }
+                .padding(.bottom)
+                .frame(maxHeight: UIScreen.main.bounds.height / 3)
+                
+                
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Text("이용약관")
+                            .bold()
+                    }
+                    
+                    Text("및")
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("개인정보 취급방침")
+                            .bold()
+                    }
+                }
+                .font(.caption)
+                .padding(.top)
+                .frame(maxHeight: UIScreen.main.bounds.height / 5)
+            }
+            
+        }
+        .foregroundColor(Color("Font"))
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
