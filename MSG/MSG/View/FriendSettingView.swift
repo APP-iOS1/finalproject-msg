4821edca7266b1d8971b3ae361fee9b2654b93f3
stealmh <kmh922@naver.com>
2023-01-17 16:03:16 +0900

    [Add] 친구관리 뷰 생성 #6

---
+++ MSG/MSG/View/FriendSettingView.swift
@@ -0,0 +1,41 @@
+//
+//  FriendSettingView.swift
+//  MSG
+//
+//  Created by kimminho on 2023/01/17.
+//
+
+import SwiftUI
+
+struct FriendSettingView: View {
+    @State private var pickerCase: PickerCase = .friend
+    
+    enum PickerCase: String,Identifiable, CaseIterable {
+        case friend
+        case alert
+        var id: String { self.rawValue }
+    }
+    var body: some View {
+        VStack {
+            Picker("gender",selection: $pickerCase) {
+                ForEach(PickerCase.allCases) {myCase in
+                    Text(myCase.rawValue.capitalized).tag(myCase)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
+            switch pickerCase {
+            case .friend:
+                FriendView()
+            case .alert:
+                AlertView()
+            }
+        }.frame(minWidth: 100, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity)
+    }
+}
+
+struct FriendSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendSettingView()
+    }
+}
