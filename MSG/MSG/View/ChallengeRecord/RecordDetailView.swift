8f5e1f53a814444956e5b1d88e2437022bbe9b39
jeongsoohee <dkssudgo365@naver.com>
2023-01-18 11:04:51 +0900

    Add: 기본 UI 작업  #18

--- MSG/MSG/View/ChallengeRecord/RecordDetailView.swift
+++ MSG/MSG/View/ChallengeRecord/RecordDetailView.swift
@@ -9,10 +9,82 @@ import SwiftUI
 
 struct RecordDetailView: View {
     var body: some View {
-        ZStack{
-            Color("Background").ignoresSafeArea()
-            VStack{
-                Text("치킨내기! 30만원으로 한달 살기")
+        NavigationStack{
+            ZStack{
+                Color("Background").ignoresSafeArea()
+                ScrollView{
+                VStack{
+                    Group{
+                        HStack{
+                            Text("치킨내기! 30만원으로 한달 살기")
+                                .font(.title3.bold())
+                                .foregroundColor(Color("Font"))
+                            Spacer()
+                        }
+                        Divider()
+                        HStack{
+                            Text("2023년01월01일 ~ 2023년01월31일")
+                                .padding(.bottom, 20)
+                            Spacer()
+                        }
+                    }.padding(.horizontal)
+                     .foregroundColor(Color("Font"))
+                    Group{
+                        ForEach(0..<5, id: \.self) { _ in
+                            HStack{
+                                Image(systemName: "person")
+                                    .font(.largeTitle)
+                                    .background(content: {
+                                        Color("Point2")
+                                    })
+                                    .padding(.trailing)
+                                Text("총 250,000원 사용")
+                                Spacer()
+                            }
+                        }
+                        
+                    }.padding([.vertical, .horizontal])
+                     .foregroundColor(Color("Font"))
+                    Group{
+                        HStack{
+                            Text("가장 적게 쓴 사람")
+                                .padding(.trailing)
+                            Image(systemName: "person")
+                                .font(.largeTitle)
+                                .background(content: {
+                                    Color("Point2")
+                                })
+                        }.padding(.top, 20)
+                        HStack{
+                            Text("가장 많이 쓴 사람")
+                                .padding(.trailing)
+                            Image(systemName: "person")
+                                .font(.largeTitle)
+                                .background(content: {
+                                    Color("Point2")
+                                })
+                        }
+                    }
+                    .padding([.vertical, .horizontal], 10)
+                    .foregroundColor(Color("Font"))
+                    
+                    }
+                    
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: Text("상세소비내역차트뷰")) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color("Point2"))
+                            .frame(width: 80, height: 45)
+                            .overlay {
+                                Text("상세내역")
+                                    .foregroundColor(Color("Font"))
+                                    .font(.subheadline)
+                            }
+                    }
+                }
             }
         }
     }
