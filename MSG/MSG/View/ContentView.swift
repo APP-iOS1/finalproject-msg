041d88af437ec3734df863a4226105a63090f927
JIN-JJS <joonsu2005@naver.com>
2023-01-26 14:59:21 +0900

    Bug/Delay MakeProfileVIew #64

--- MSG/MSG/View/ContentView.swift
+++ MSG/MSG/View/ContentView.swift
@@ -35,6 +35,9 @@ struct ContentView: View {
                         if loginViewModel.currentUserProfile == nil {
                             withAnimation {
                                 MakeProfileView()
+                                // MakeProfileView를 지연 시킴
+                                    .deferredRendering(for: 0.5
+                                )
                             }
                             
                         } else {
@@ -116,5 +119,46 @@ struct ContentView_Previews: PreviewProvider {
             .environmentObject(fireStoreViewModel)
             .environmentObject(realtimeViewModel)
             .environmentObject(NotificationManager())
+    }
+}
+
+
+// MARK: 123번 ~ 161번 지연 시키는 ViewModifier
+private struct DeferredViewModifier: ViewModifier {
+
+    // MARK: API
+
+    let threshold: Double
+
+    // MARK: - ViewModifier
+
+    func body(content: Content) -> some View {
+        _content(content)
+            .onAppear {
+               DispatchQueue.main.asyncAfter(deadline: .now() + threshold) {
+                   self.shouldRender = true
+               }
+            }
+    }
+
+    // MARK: - Private
+
+    @ViewBuilder
+    private func _content(_ content: Content) -> some View {
+        if shouldRender {
+            content
+        } else {
+            content
+                .hidden()
+        }
+    }
+
+    @State
+    private var shouldRender = false
+}
+
+extension View {
+    func deferredRendering(for seconds: Double) -> some View {
+        modifier(DeferredViewModifier(threshold: seconds))
     }
 }
