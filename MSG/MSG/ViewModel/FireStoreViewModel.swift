f2eab76ca20b5fc82fd898aa51002db5435adac8
zooey <popularyg@gmail.com>
2023-01-26 12:06:04 +0900

    Feat: View에 지출내역 불러오기 #84

--- MSG/MSG/ViewModel/FireStoreViewModel.swift
+++ MSG/MSG/ViewModel/FireStoreViewModel.swift
@@ -227,7 +227,7 @@ class FireStoreViewModel: ObservableObject {
             self.expenditure = expenditure
             print(expenditure)
         } catch {
-            
+            print("catched")
         }
     }
     
