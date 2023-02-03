54eddb0c464163232bc662de06c11f02593c809b
Sehoon Jeong <wtd120120@gmail.com>
2023-02-03 11:38:35 +0900

    [Feat]: TextModifier 추가

--- MSG/MSG/ViewModifier.swift
+++ MSG/MSG/ViewModifier.swift
@@ -25,23 +25,60 @@ struct TextViewModifier: ViewModifier{
     }
 }
 
+
+struct FontCustomType{
+    static var largeTitle = (fontSize:32, fontStyle: Font.TextStyle.title)
+    static var title = (fontSize:28, fontStyle: Font.TextStyle.title)
+    static var title2 = (fontSize:22, fontStyle: Font.TextStyle.title)
+    static var title3 = (fontSize:20, fontStyle: Font.TextStyle.title)
+    static var body = (fontSize:17, fontStyle: Font.TextStyle.title)
+    static var callout = (fontSize:16, fontStyle: Font.TextStyle.title)
+    static var subhead = (fontSize:15, fontStyle: Font.TextStyle.title)
+    static var footnot = (fontSize:13, fontStyle: Font.TextStyle.title)
+    static var caption = (fontSize:12, fontStyle: Font.TextStyle.title)
+    static var caption2 = (fontSize:11, fontStyle: Font.TextStyle.title)
+}
+
+enum FontCustomColor: String{
+    case color1 = "Color1"
+    case color2 = "Color2"
+}
+
+enum FontCustomWeight:String {
+    case normal = "MaplestoryOTFLight"
+    case bold = "MaplestoryOTFBold"
+}
+
+struct TextModifier: ViewModifier{
+    let fontWe: FontCustomWeight
+    let fontTy: (fontSize: CGFloat, fontStyle: Font.TextStyle)
+    let color:FontCustomColor
+    
+    func body(content: Content) -> some View {
+            content
+            .foregroundColor(Color(color.rawValue))
+                .font(.custom(fontWe.rawValue, size: fontTy.fontSize, relativeTo: fontTy.fontStyle))
+    }
+}
+
+
 struct TextTitleBold: ViewModifier{
     let size = 50
     let height = UIScreen.main.bounds.size.height
-    
+
     func dynamicFontSize(_ FontSize: CGFloat) -> CGFloat {
         let screenWidth = UIScreen.main.bounds.size.width
         let calculatedFontSize = screenWidth / height * FontSize
         return calculatedFontSize
     }
-    
+
     func body(content: Content) -> some View {
 
             content
                 .foregroundColor(Color("Color2"))
                 .font(.custom("MaplestoryOTFBold", size: dynamicFontSize(CGFloat(size))))
 
-            
+
     }
 }
 
@@ -67,3 +104,4 @@ struct TextTitleSemiBold: ViewModifier{
 }
 
 
+    
