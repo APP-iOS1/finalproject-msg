c593bcb8794117b8c1d76c8be159da33ea309440
zooey <popularyg@gmail.com>
2023-02-09 13:03:32 +0900

    Feat: FriendView 뱃지 #250

--- MSG/MSG/View/FriendSetting/FriendSettingView.swift
+++ MSG/MSG/View/FriendSetting/FriendSettingView.swift
@@ -10,34 +10,69 @@ import SwiftUI
 struct FriendSettingView: View {
     @Binding var selectedTabBar: SelectedTab
     @EnvironmentObject var firebaseViewModel: FireStoreViewModel
-//    @Binding
     @State var selection: Int = 0
     let titles: [String] = ["친구", "친구요청", "도전장"]
     
+    @Binding var friendCount: Int
+    @Binding var requsetCount: Int
+    func numOfDigits() -> Float {
+        let numOfDigits = Float(String(friendCount + requsetCount).count)
+        return numOfDigits == 1 ? 1.5 : numOfDigits
+    }
+    
     var body: some View {
         
-        ZStack {
-            Color("Color1")
-                .ignoresSafeArea()
-            VStack {
-                SegementedControllView(selection: $selection, titles: titles, selectedItemColor: Color("Color2"), backgroundColor: Color(.clear), selectedItemFontColor: Color("Color2"))
-//                    .modifier(TextViewModifier(color: "Color2"))
-                
-                if selection == 0 {
-                    DivideFriendView(findFriendToggle: .constant(false))
-                } else if selection == 1 {
-                    AlertView()
-                } else {
-                    GameRequestAlertView(selectedTabBar: $selectedTabBar)
+        GeometryReader { g in
+            ZStack {
+                Color("Color1")
+                    .ignoresSafeArea()
+                VStack {
+                    SegementedControllView(selection: $selection, titles: titles, selectedItemColor: Color("Color2"), backgroundColor: Color(.clear), selectedItemFontColor: Color("Color2"))
+                    
+                    if selection == 0 {
+                        DivideFriendView(findFriendToggle: .constant(false))
+                    } else if selection == 1 {
+                        AlertView()
+                    } else {
+                        GameRequestAlertView(selectedTabBar: $selectedTabBar)
+                    }
+                }
+                .overlay{
+                    if friendCount != 0 {
+                        ZStack {
+                            Capsule()
+                                .fill(.red)
+                                .frame(width: g.size.width / 31 * CGFloat(numOfDigits()), height: g.size.height / 36)
+                                .position(CGPoint(x: g.size.width / 1.65, y: g.size.height / 20))
+                            Text("\(friendCount)")
+                                .foregroundColor(Color.white)
+                                .modifier(TextModifier(fontWeight: FontCustomWeight.bold, fontType: FontCustomType.caption, color: FontCustomColor.color2))
+                                .position(CGPoint(x: g.size.width / 1.65, y: g.size.height / 20))
+                        }
+                    }
+                }
+                .overlay{
+                    if requsetCount != 0 {
+                        ZStack {
+                            Capsule()
+                                .fill(.red)
+                                .frame(width: g.size.width / 31 * CGFloat(numOfDigits()), height: g.size.height / 36)
+                                .position(CGPoint(x: g.size.width / 1.11, y: g.size.height / 20))
+                            Text("\(requsetCount)")
+                                .foregroundColor(Color.white)
+                                .modifier(TextModifier(fontWeight: FontCustomWeight.bold, fontType: FontCustomType.caption, color: FontCustomColor.color2))
+                                .position(CGPoint(x: g.size.width / 1.11, y: g.size.height / 20))
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-//struct FriendSettingView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        FriendSettingView()
-//    }
-//}
+struct FriendSettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendSettingView(selectedTabBar: .constant(.third), friendCount: .constant(1), requsetCount: .constant(1))
+    }
+}
 
